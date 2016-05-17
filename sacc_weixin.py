691885b5ee623b70793d7b8218b2bdd84002dc0f
Neo0 <wjh100@live.cn>
2016-05-17 11:11:40 +0800

    sacc_weixin completed

---
+++ sacc_weixin.py
@@ -0,0 +1,53 @@
+# _*_ coding:utf-8 _*_
+
+import hashlib
+from xml.etree.ElementTree import fromstring
+import time
+from flask import Flask, request, make_response
+import sys
+import urllib, httplib
+
+app = Flask(__name__)
+# app.debug = True
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def weixinoauth():
+    if request.method == 'GET':
+        token = 'weixin'
+        query = request.args
+        sig = query.get('signature', '')
+        timesamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timesamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if hashlib.sha1(s).hexdigest() == sig:
+            return make_response(echostr)
+    else:
+        request.setCharacterEncoding("UTF-8")
+        recv = request.stream.read()
+        xml_recv = fromstring(recv)
+        ToUserName = xml_recv.find('ToUserName').text
+        FromUserName = xml_recv.find('FromUserName').text
+        key = xml_recv.find("Content").text
+        zccx = key.split(' ')
+        url = 'http://127.0.0.1:5000/'
+        postp = {'name': zccx[0], 'id': zccx[1]}
+        params = urllib.urlencode(postp)
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        conn = httplib.HTTPConnection(url)
+        conn.request("POST", '/', params, headers)
+        res = conn.getresponse().read()
+        conn.close()
+
+        reply = '''<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>'''
+        response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), res))
+        response.content_type = 'application/xml'
+        response.setCharacterEncoding("UTF-8")
+        return response
